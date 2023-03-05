b09d0b58f1174671da43f4f6c5c0933bacc330f8
Elena <Esavvina51@gmail.com>
2023-03-06 08:46:52 +0900

    Initial Commit

---
+++ Yummi_example/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Yummi_example
+//
+//  Created by Саввина Елена on 06.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
