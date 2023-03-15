16de33700cd4f856f88e57eddb6d3119244b00d1
Elena <Esavvina51@gmail.com>
2023-03-15 20:09:44 +0900

    session3

---
+++ ро/MyCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  MyCollectionViewCell.swift
+//  ро
+//
+//  Created by Саввина Елена on 15.03.2023.
+//
+
+import UIKit
+
+class MyCollectionViewCell: UICollectionViewCell {
+    
+    
+    @IBOutlet weak var imagerec: UIImageView!
+}
--- ро/ViewController.swift
+++ ро/ViewController.swift
@@ -7,8 +7,20 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return imagerec.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! MyCollectionViewCell
+        cell.imagerec.image = UIImage(named: imagerec[indexPath.row])
+        return cell
+    }
+    
 
+    var imagerec: [String] = ["Banner 1", "banner"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
